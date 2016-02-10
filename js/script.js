afbb00f5cdc2eb047b90e2a103b47a0c18650c18
Kristin Annabel Folland <kristinannabel.folland@gmail.com>
2016-02-10 13:54:25 +0100

    Moved the javaScript-code to the js-file, instead of index. Cleaned up some tabs etc.

--- js/script.js
+++ js/script.js
@@ -1,161 +1,176 @@
-$(document).ready(function(){
-	/*$(document).on("click", ".decompose", function(e) {
-		//e.preventDefault();
-		setTablesCanvas();
-    });
-	/*$lastImageH = 0;
-	$lastImageW = 0;
-	$totalImageH = 0;
-	var picObjArr = [];
-	var renderer;
-	var stage;
-	
-	
-	$('.decompose').click(function(e){
-		debugger;
-		var canvas = $("#canvas");
-		renderer = PIXI.autoDetectRenderer(600,600, canvas);
-		renderer.backgroundColor = 0xffffff;
-		document.body.appendChild(renderer.view);
-		stage = new PIXI.Container();
-		addImage('/viSQLizer/Course/code.png', 'code', false, true);
-		addImage('/viSQLizer/Course/name.png', 'name', false, false);
-		draw();
-	});
-	//setSizeOfCanvas(picObjArr);
-	
-	function setSizeOfCanvas($array){
-		debugger;
-		var finalWidth = 0;
-		var finalHeight = 0;
-		var thisObjH = 0;
-		var prevObjH = 0;
-		for(var i = 0; i < $array.length; i++){
-			debugger;
-			finalWidth += $array[i].width;
-			thisObjH = $array[i].height;
-			if(thisObjH >= prevObjH){
-				finalHeight = thisObjH;
-			}
-			prevObjH = thisObjH;
-		}
-	}
-	
-	function setPositionOfElement($elem,$posx,$posy){
-		$elem.position.x = $posx;
-		$elem.position.y = $posy;
-	}
-	
-	function addImage($url, $name, $newRow, $isFirst){
-		var texture = PIXI.Texture.fromImage($url);
-		var img = new PIXI.Sprite(texture);
-		picObjArr.push(img);
-		if(!$isFirst && !$newRow){//not first elem and not new row
-			var posx = $lastImageW + 2;
-			//var posy = $lastImageH + 2;
-			setPositionOfElement(img, posx, 0);
-		}
-		else if($isFirst && $newRow){//first elem of new row
-			$totalImageH += $lastImageH;
-			//var posx = $lastImageW + 2;
-			var posy = $totalImageH + 2;
-			setPositionOfElement(img, 0, posy);
-		}
-		else if(!$isFirst && $newRow){//not first but new row
-			var posx = $lastImageW + 2;
-			var posy = $totalImageH + 2;
-			setPositionOfElement(img, posx, posy);
-			
-		}
-		stage.addChild(img);
-		var thisImg = $($name);
+function init() {
+	if($(".streammode-panel").length != 0){
 		
-		$lastImageW = thisImg.width() + 2;
-		$lastImageH = thisImg.height() + 2;
+    	var stage = new createjs.Stage("demoCanvas");
+		var canvas = document.getElementById("demoCanvas");
+		var table = document.getElementById("main-panel streammode-panel");
+		var formDOMElement = new createjs.DOMElement("main-panel streammode-panel");
+
+  		stage.canvas.width = formDOMElement.htmlElement.clientWidth + 2;
+		stage.canvas.height = formDOMElement.htmlElement.clientHeight + 2;
+		//move it's rotation center at the center of the form
+		formDOMElement.regX = table.offsetWidth*0.5;
+		formDOMElement.regY = table.offsetHeight*0.5;
+		//move the form above the screen
+		formDOMElement.x = canvas.width * 0.5;
+		formDOMElement.y = canvas.height * -0.50;
+
+    	stage.addChild(formDOMElement);
+    	stage.update();
+  		var thCount = $(".empty-table").children('tbody').find('tr').first().find("th").length;
+		for(var i = 0; i < thCount; i++){
+			var spanName = ".span_" + i;
+			$(".empty-table").find(spanName).css("visibility","hidden");
+			$(".empty-table").find(".textOrigin").hide();
+			var numOfSpans = $(".empty-table").find(spanName).length;
+			for(var j = 0; j < numOfSpans; j++){
+				while ($(".empty-table").find(spanName).eq(j).height()>16) {
+			    	$(".empty-table").find(spanName).eq(j).text(function (index, text) {
+			        	return text.replace(/\W*\s(\S)*$/, '...');
+			    	});
+				}
+			}			
+		}
+		var numberOfTables = $(".original-table").length;
+		var tableRows = $("#empty-table").find("tr.data").length;
+		var tableColumns = $("#empty-table").find("tr.data:first").find("td").length;
+		var timeCount = 1500;
+		for(var i = 0; i < tableRows; i++) (function(i){ //for each row
+			for(var j = 0; j < tableColumns; j++) (function(j){ //for each column in one row
+				var rowCount = i + 2;
+				var columnCount = j + 1;
+				var textContent = $("#empty-table").find("tr:nth-child("+rowCount+")").find("td:nth-child("+columnCount+")").find("span.textOrigin").html();
+				var emptyContains = $("#empty-table").find("span:contains('"+textContent+"')").length;
+						
+				var numOfThisElem = $("#empty-table").find("tr:nth-child("+rowCount+")").find("span.textOrigin:contains('"+textContent+"')").length;
+				var numOfThisElemNotUsed = $("#empty-table").find("tr:nth-child("+rowCount+")").find("span.textOrigin:not(.used):contains('"+textContent+"')").length;
+				var elemNumInRow = numOfThisElem - numOfThisElemNotUsed;
+				$("#empty-table").find("tr:nth-child("+rowCount+")").find("span.textOrigin:contains('"+textContent+"')").eq(elemNumInRow).addClass("used");
+						
+				if(elemNumInRow > 0){
+							var thisGetTextOrigin = $(".original-table").find("span:not(.used):not(.duplicate)").filter(function(){
+									return $(this).html() === textContent;
+							});
+						}
+				else {
+							var thisGetTextOrigin = $(".original-table").find("span:not(.used)").filter(function(){
+									return $(this).html() === textContent;
+							});
+						}
+						
+				var isRightElem = false;
+				var thisTable = thisGetTextOrigin.first().parent().parent().parent().parent();
+				var duplData = $(thisTable).find("span:not(.used):contains('"+textContent+"')").length;
+				var columnIndexOriginal = thisGetTextOrigin.first().parent().index();
+				var columnIndexEmpty = $("#empty-table").find("tr:nth-child("+rowCount+")").find("td:nth-child("+columnCount+")").index();
+				var isNotFirstColumn = false;
+				if((columnIndexOriginal > 0)&&(columnIndexEmpty > 0)){
+							isNotFirstColumn = true;
+						}
+						
+				var nextEmptyTableColumn = $("#empty-table").find("tr:nth-child("+rowCount+")").find("td:nth-child("+columnCount+")").parent().parent().children().first().find("th").eq(columnIndexEmpty-1).html();
+				var nextOriginalTableColumn = thisGetTextOrigin.first().parent().parent().parent().children().first().find("th").eq(columnIndexOriginal-1).html();
+						
+				if(duplData > 1){
+							while(isRightElem == false){
+								if(thisGetTextOrigin.first().parent().is(':first-child')){
+									var getTextOrigin = $(thisGetTextOrigin[0]).attr("id","animThis").next();
+									isRightElem = true;
+								}
+								else if(thisGetTextOrigin.first().parent().parent().has(".usedInRow").length != 0){
+									var getTextOrigin = $(thisGetTextOrigin[0]).attr("id","animThis").next();
+									isRightElem = true;
+								}
+								else if((!thisGetTextOrigin.first().parent().siblings().find("span:not(#original-span)").hasClass("used")) && ((isNotFirstColumn) && (nextEmptyTableColumn != nextOriginalTableColumn))){
+									//This element is first to be used in row even if it is not first element
+									var getTextOrigin = $(thisGetTextOrigin[0]).attr("id","animThis").next();
+									isRightElem = true;
+								}
+								else{
+									thisGetTextOrigin.first().addClass("notInUse");
+									var thisGetTextOrigin = $(".original-table").find("span:not(.used):not(.notInUse)").filter(function(){
+										return $(this).html() === textContent;
+									});
+								}
+							}
+						}
+				else {
+					var getTextOrigin = $(thisGetTextOrigin[0]).attr("id","animThis").next();
+				}
+						
+				var originalPosY = $(".original-table").find("span#animThis").first().position().top;
+						
+				var emptyPosY = $("#empty-table").find("tr:nth-child("+rowCount+")").find("td:nth-child("+columnCount+")").find("span:not(.textOrigin)").position().top;
+				var emptyPosX = $("#empty-table").find("tr:nth-child("+rowCount+")").find("td:nth-child("+columnCount+")").find("span:not(.textOrigin)").position().left;
+				if(numberOfTables > 1){
+							var thisTableTemp = $(".original-table").find("span#animThis").parent().parent().parent().parent().attr("id");
+							var thisTable = "#" + thisTableTemp;
+							var orPosX = $(thisTable).find("span#animThis").position().left;
+						}
+				else {
+							var orPosX = 									$(".original-table").find("tr:nth-child("+rowCount+")").find("td:nth-child("+columnCount+")").find("span").position().left;
+						}
+				var calcPositionY = emptyPosY - originalPosY;
+				var calcPositionX = emptyPosX - orPosX;
+				var thisDOMElem = $("#animThis:not(.used)").get(0);
+				var textDOM = new createjs.DOMElement(thisDOMElem);
+				stage.addChild(textDOM);
+				if(numberOfTables > 1){
+							getTextOrigin.show();
+			  		  		createjs.Tween.get(textDOM, {loop: false})
+							.wait(timeCount).call(tweenStart)
+							.to({y: calcPositionY, x: calcPositionX}, 1500, createjs.Ease.getPowIn(1))
+							.to({alpha: 0}, 0, createjs.Ease.getPowIn(1))
+							.to({alpha: 1, y: 0, x: 0}).call(tweenComplete);
+						} else {
+			  		  		createjs.Tween.get(textDOM, {loop: false})
+							.wait(timeCount).call(tweenStart)
+							.to({y: calcPositionY, x: calcPositionX}, 1500, createjs.Ease.getPowIn(1));
+							//console.log($("#empty-table").find("tr:nth-child("+rowCount+")").find("td:nth-child("+columnCount+")").find("span").position());
+							getTextOrigin.show();
+						}
+						
+				if(numberOfTables > 1){
+							var thisTableIdTemp = $(".original-table").find("span#animThis").parent().parent().parent().parent().attr("id");
+							var thisTableId = "#" +thisTableIdTemp;
+							var countOfDuplicates = $(thisTableId).find("span:not(#original-span):contains('"+textContent+"')").length;
+							if(countOfDuplicates > 1){
+								$(".original-table").find("span#animThis").removeAttr("id").addClass("used").parent().addClass("usedInRow");
+							}else{
+								var duplicatesInAllTables = $(".original-table").find("span:not(.used):contains('"+textContent+"')").length;
+								if(duplicatesInAllTables > countOfDuplicates){
+									$(".original-table").find("span#animThis").first().addClass("duplicate").parent().addClass("usedInRow");
+								}
+								$(".original-table").find("span#animThis").first().removeAttr("id").parent().addClass("usedInRow");
+							}
+						}
+				else {
+					$(".original-table").find("span#animThis").removeAttr("id").addClass("used").parent().addClass("usedInRow");
+				}
+						
+				function tweenComplete(){
+					var emptyTextPlace = $("#empty-table").find("tr:nth-child("+rowCount+")").find("td:nth-child("+columnCount+")").find("span");
+					emptyTextPlace.css("visibility", "visible");
+				}
+						
+				function tweenStart(){
+					$(this.htmlElement.parentElement.parentElement).css("background-color", "#d9edf7");
+				}
+						
+			})(j);
+			timeCount += 2000;
+			$(".original-table").find(".usedInRow").removeClass("usedInRow");
+		})(i);
+			
+        createjs.Ticker.setFPS(60);
+        createjs.Ticker.addEventListener("tick", stage);
 	}
-	
-	function draw() {
-	  renderer.render(stage);
-	  requestAnimationFrame(draw);
-	}*/
-	//draw();
-	//renderer.render(stage);
-	/*var sprite = PIXI.Sprite.fromImage("/viSQLizer/Course/code.png");
-	var graphics = new PIXI.Graphics();
-	stage.addChild(graphics);
-	graphics.addChild(sprite);
-	renderer.render(stage);*
+}
+
+$( document ).ready(function() {
 	
 	/** Operations for stream mode **/
 	
-	
-	//$('input[name="pagemode"]:checked').prop("disabled", true);
-	function setTableCanvas(){
-			if($(".streammode-panel").length != 0){ //do not do this until a table is in the DOM
-	        	var stage = new createjs.Stage("demoCanvas");
-	  			var canvas = document.getElementById("demoCanvas");
-	  			var table = document.getElementById("main-panel streammode-panel");
-	  			var formDOMElement = new createjs.DOMElement("main-panel streammode-panel");
-
-	  			//move it's rotation center at the center of the form
-	  			formDOMElement.regX = table.offsetWidth*0.5;
-	  			formDOMElement.regY = table.offsetHeight*0.5;
-				
-	  	  		stage.canvas.width = formDOMElement.htmlElement.clientWidth + 2;
-	  			stage.canvas.height = formDOMElement.htmlElement.clientHeight + 2;
-	  			//move the form above the screen
-	  			formDOMElement.x = canvas.width * 0.5;
-	  			formDOMElement.y = canvas.height * -0.50;
-
-	        	stage.addChild(formDOMElement);
-	        	stage.update();
-	  	  		var thCount = $(".empty-table").children('tbody').find('tr').first().find("th").length;
-	  			for(var i = 0; i < thCount; i++){
-	  				var spanName = ".span_" + i;
-	  				$(".empty-table").find(spanName).css("visibility","hidden");
-	  			}
-			}
-			if($(".org-db-table").length != 0){ // do not do this if the shown table is the original DB table, not result table
-				debugger;
-				var tableRows = $("#empty-table").find("tr.data").length;
-				var tableColumns = $("#empty-table").find("tr.data:first").find("td").length;
-				var timeCount = 1500;
-					for(var i = 0; i < tableRows; i++){ //for each row
-						for(var j = 0; j < tableColumns; j++){ //for each column in one row
-							//debugger;
-							var rowCount = i + 2;
-							var columnCount = j + 1;
-							var textContent = $("#empty-table").find("tr:nth-child("+rowCount+")").find("td:nth-child("+columnCount+")").find("span").html();
-							var getTextOrigin = $(".original-table").find("span").filter(function() {
-								// Matches exact string   
-								return $(this).html() === textContent;
-							}).first().attr("id","animThis").next();
-							//$(".original-table").find('span:contains("'+textContent+'"):first').attr("id","animThis").next();
-							
-							var originalPos = $(".original-table").find("span").filter(function() {
-																// Matches exact string   
-																return $(this).html() === textContent;
-															}).first().position().top;
-							//$(".original-table").find('span:contains("'+textContent+'"):first').position().top;
-							var emptyPos = $("#empty-table").find("tr:nth-child("+rowCount+")").find("td:nth-child("+columnCount+")").find("span").position().top;
-							var calcPosition = emptyPos - originalPos;
-							var textDOM = new createjs.DOMElement("animThis");
-							stage.addChild(textDOM);
-				  		  	createjs.Tween.get(textDOM, {loop: false})
-							.wait(timeCount)
-							.to({y: calcPosition}, 1000, createjs.Ease.getPowIn(1));
-							//console.log($("#empty-table").find("tr:nth-child("+rowCount+")").find("td:nth-child("+columnCount+")").find("span").position());
-							getTextOrigin.show();
-							$(".original-table").find("span").filter(function() {
-																// Matches exact string   
-																return $(this).html() === textContent;
-															}).first().removeAttr("id");
-						}
-						timeCount += 1500;
-					}
-			}
-		
-	}
 	function setActiveStep(anum){
 		var thisStep = ".step" + anum;
 		$(".wizard-footer").find(thisStep).addClass("active");
