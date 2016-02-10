e3b14d4601cc91b55bccd021d9b23bbd5d26ddaf
Kristin Annabel Folland <kristinannabel.folland@gmail.com>
2016-02-10 15:57:11 +0100

    Code completely commented

--- js/visualization.js
+++ js/visualization.js
@@ -21,6 +21,63 @@ function setViewToEmptyTable(thCount){
 }
 
 /*
+ * Function for checking if data is duplicated in the specific original-table
+ * Checks if the right element is sent, and not just first occurence of similar element
+ */
+function checkIfDuplicatedData(textContent, duplData, thisGetTextOrigin, rowCount, columnCount){
+	var isRightElem = false;
+	
+	// Column index of this element
+	var columnIndexOriginal = thisGetTextOrigin.first().parent().index();
+	
+	// Column index of this element in empty table
+	var columnIndexEmpty = $("#empty-table").find("tr:nth-child("+rowCount+")").find("td:nth-child("+columnCount+")").index();
+	var isNotFirstColumn = false;
+	
+	if((columnIndexOriginal > 0)&&(columnIndexEmpty > 0)){
+		// If this is not the first column in both tables
+		isNotFirstColumn = true;
+	}
+	
+	// The previous column's name in empty-table
+	var prevEmptyTableColumn = $("#empty-table").find("tr:nth-child("+rowCount+")").find("td:nth-child("+columnCount+")").parent().parent().children().first().find("th").eq(columnIndexEmpty-1).html();
+	// The previous column's name in original-table
+	var prevOriginalTableColumn = thisGetTextOrigin.first().parent().parent().parent().children().first().find("th").eq(columnIndexOriginal-1).html();
+	
+	// If this element is duplicate in original table
+	if(duplData > 1){
+				while(isRightElem == false){
+					if(thisGetTextOrigin.first().parent().is(':first-child')){
+						// If this is the first element in this row
+						isRightElem = true;
+						return $(thisGetTextOrigin[0]).attr("id","animThis").next();
+					}
+					else if(thisGetTextOrigin.first().parent().parent().has(".usedInRow").length != 0){
+						// If this has siblings with class usedInRow, siblings that have been used already on this row
+						isRightElem = true;
+						return $(thisGetTextOrigin[0]).attr("id","animThis").next();
+					}
+					else if((!thisGetTextOrigin.first().parent().siblings().find("span:not(#original-span)").hasClass("used")) && ((isNotFirstColumn) && (prevEmptyTableColumn != prevOriginalTableColumn))){
+						// If this element is first to be used in row even if it is not first element
+						isRightElem = true;
+						return $(thisGetTextOrigin[0]).attr("id","animThis").next(); //TEST THIS; SEEMS WRONG IF isNotFirstColumn is false???
+					}
+					else{
+						// This element is not the right one, find next occurence
+						thisGetTextOrigin.first().addClass("notInUse");
+						var thisGetTextOrigin = $(".original-table").find("span:not(.used):not(.notInUse)").filter(function(){
+							return $(this).html() === textContent;
+						});
+					}
+				}
+			}
+	else {
+		// If this element is alone in this table, it is the only possible element to choose
+		return $(thisGetTextOrigin[0]).attr("id","animThis").next();
+	}
+}
+
+/*
  * Initial function for the animation and canvas visualizations
  * Is run onload of DOM body
  */
@@ -94,112 +151,112 @@ function init() {
 					});
 				}
 						
-				var isRightElem = false;
 				var thisTable = thisGetTextOrigin.first().parent().parent().parent().parent();
 				var duplData = $(thisTable).find("span:not(.used):contains('"+textContent+"')").length;
 				
-				
-				var columnIndexOriginal = thisGetTextOrigin.first().parent().index();
-				var columnIndexEmpty = $("#empty-table").find("tr:nth-child("+rowCount+")").find("td:nth-child("+columnCount+")").index();
-				var isNotFirstColumn = false;
-				if((columnIndexOriginal > 0)&&(columnIndexEmpty > 0)){
-							isNotFirstColumn = true;
-						}
+				// The element to be shown in original table when animation begins. The current element to be animated get id animThis in this function
+				var getTextOrigin = checkIfDuplicatedData(textContent, duplData, thisGetTextOrigin, rowCount, columnCount);
 						
-				var nextEmptyTableColumn = $("#empty-table").find("tr:nth-child("+rowCount+")").find("td:nth-child("+columnCount+")").parent().parent().children().first().find("th").eq(columnIndexEmpty-1).html();
-				var nextOriginalTableColumn = thisGetTextOrigin.first().parent().parent().parent().children().first().find("th").eq(columnIndexOriginal-1).html();
-						
-				if(duplData > 1){
-							while(isRightElem == false){
-								if(thisGetTextOrigin.first().parent().is(':first-child')){
-									var getTextOrigin = $(thisGetTextOrigin[0]).attr("id","animThis").next();
-									isRightElem = true;
-								}
-								else if(thisGetTextOrigin.first().parent().parent().has(".usedInRow").length != 0){
-									var getTextOrigin = $(thisGetTextOrigin[0]).attr("id","animThis").next();
-									isRightElem = true;
-								}
-								else if((!thisGetTextOrigin.first().parent().siblings().find("span:not(#original-span)").hasClass("used")) && ((isNotFirstColumn) && (nextEmptyTableColumn != nextOriginalTableColumn))){
-									//This element is first to be used in row even if it is not first element
-									var getTextOrigin = $(thisGetTextOrigin[0]).attr("id","animThis").next();
-									isRightElem = true;
-								}
-								else{
-									thisGetTextOrigin.first().addClass("notInUse");
-									var thisGetTextOrigin = $(".original-table").find("span:not(.used):not(.notInUse)").filter(function(){
-										return $(this).html() === textContent;
-									});
-								}
-							}
-						}
-				else {
-					var getTextOrigin = $(thisGetTextOrigin[0]).attr("id","animThis").next();
-				}
-						
+				// Y position of the element in the original table
 				var originalPosY = $(".original-table").find("span#animThis").first().position().top;
 						
+				// Y position of the element in the empty-table
 				var emptyPosY = $("#empty-table").find("tr:nth-child("+rowCount+")").find("td:nth-child("+columnCount+")").find("span:not(.textOrigin)").position().top;
+				// X position of the element in the empty-table
 				var emptyPosX = $("#empty-table").find("tr:nth-child("+rowCount+")").find("td:nth-child("+columnCount+")").find("span:not(.textOrigin)").position().left;
+				
+				// If there is more than one original-table
 				if(numberOfTables > 1){
-							var thisTableTemp = $(".original-table").find("span#animThis").parent().parent().parent().parent().attr("id");
-							var thisTable = "#" + thisTableTemp;
-							var orPosX = $(thisTable).find("span#animThis").position().left;
-						}
+					// Get the right table for the element
+					var thisTableTemp = $(".original-table").find("span#animThis").parent().parent().parent().parent().attr("id");
+					var thisTable = "#" + thisTableTemp;
+					var orPosX = $(thisTable).find("span#animThis").position().left;
+				}
 				else {
-							var orPosX = 									$(".original-table").find("tr:nth-child("+rowCount+")").find("td:nth-child("+columnCount+")").find("span").position().left;
-						}
+					var orPosX = $(".original-table").find("tr:nth-child("+rowCount+")").find("td:nth-child("+columnCount+")").find("span").position().left;
+				}
+				
+				// Calculated position Y from the original position and result position
 				var calcPositionY = emptyPosY - originalPosY;
+				// Calculated position X from the original position and result position
 				var calcPositionX = emptyPosX - orPosX;
+				
 				var thisDOMElem = $("#animThis:not(.used)").get(0);
+				//Create an DOMElement for the canvas
 				var textDOM = new createjs.DOMElement(thisDOMElem);
 				stage.addChild(textDOM);
+				
+				// If there is more than one original-table
 				if(numberOfTables > 1){
-							getTextOrigin.show();
-			  		  		createjs.Tween.get(textDOM, {loop: false})
-							.wait(timeCount).call(tweenStart)
-							.to({y: calcPositionY, x: calcPositionX}, 1500, createjs.Ease.getPowIn(1))
-							.to({alpha: 0}, 0, createjs.Ease.getPowIn(1))
-							.to({alpha: 1, y: 0, x: 0}).call(tweenComplete);
-						} else {
-			  		  		createjs.Tween.get(textDOM, {loop: false})
-							.wait(timeCount).call(tweenStart)
-							.to({y: calcPositionY, x: calcPositionX}, 1500, createjs.Ease.getPowIn(1));
-							//console.log($("#empty-table").find("tr:nth-child("+rowCount+")").find("td:nth-child("+columnCount+")").find("span").position());
-							getTextOrigin.show();
-						}
-						
+					// Show the text in the original-table
+					getTextOrigin.show();
+					// Animate the DOMElement in position set
+			  		createjs.Tween.get(textDOM, {loop: false})
+					.wait(timeCount).call(tweenStart)
+					.to({y: calcPositionY, x: calcPositionX}, 1500, createjs.Ease.getPowIn(1))
+					.to({alpha: 0}, 0, createjs.Ease.getPowIn(1))
+					.to({alpha: 1, y: 0, x: 0}).call(tweenComplete);
+					// call to dunction tweenComplete after animation is completed
+				} else {
+					// Animate the DOMElement in position set
+			  		createjs.Tween.get(textDOM, {loop: false})
+					.wait(timeCount).call(tweenStart)
+					.to({y: calcPositionY, x: calcPositionX}, 1500, createjs.Ease.getPowIn(1));
+					getTextOrigin.show();
+				}
+				
+				// If there is more than one original-table
 				if(numberOfTables > 1){
-							var thisTableIdTemp = $(".original-table").find("span#animThis").parent().parent().parent().parent().attr("id");
-							var thisTableId = "#" +thisTableIdTemp;
-							var countOfDuplicates = $(thisTableId).find("span:not(#original-span):contains('"+textContent+"')").length;
-							if(countOfDuplicates > 1){
-								$(".original-table").find("span#animThis").removeAttr("id").addClass("used").parent().addClass("usedInRow");
-							}else{
-								var duplicatesInAllTables = $(".original-table").find("span:not(.used):contains('"+textContent+"')").length;
-								if(duplicatesInAllTables > countOfDuplicates){
-									$(".original-table").find("span#animThis").first().addClass("duplicate").parent().addClass("usedInRow");
-								}
-								$(".original-table").find("span#animThis").first().removeAttr("id").parent().addClass("usedInRow");
-							}
+					var thisTableIdTemp = $(".original-table").find("span#animThis").parent().parent().parent().parent().attr("id");
+					var thisTableId = "#" +thisTableIdTemp;
+					var countOfDuplicates = $(thisTableId).find("span:not(#original-span):contains('"+textContent+"')").length;
+					// If there is duplicates of this element in this table
+					if(countOfDuplicates > 1){
+						$(".original-table").find("span#animThis").removeAttr("id").addClass("used").parent().addClass("usedInRow");
+					}
+					else{
+						var duplicatesInAllTables = $(".original-table").find("span:not(.used):contains('"+textContent+"')").length;
+						// If there is duplicates of this element in both original-tables together
+						if(duplicatesInAllTables > countOfDuplicates){
+							//This is a duplicate, mark as one
+							$(".original-table").find("span#animThis").first().addClass("duplicate").parent().addClass("usedInRow");
 						}
+						// remove id animThis from element
+						$(".original-table").find("span#animThis").first().removeAttr("id");
+					}
+				}
 				else {
+					// Element is used and used in row
 					$(".original-table").find("span#animThis").removeAttr("id").addClass("used").parent().addClass("usedInRow");
 				}
-						
+				
+				/**
+				 * Function for when animation is completed
+				 * Shows the hidden text in empty-table
+				 */
 				function tweenComplete(){
 					var emptyTextPlace = $("#empty-table").find("tr:nth-child("+rowCount+")").find("td:nth-child("+columnCount+")").find("span");
 					emptyTextPlace.css("visibility", "visible");
 				}
-						
+				
+				/**
+				 * Function for when tween is starting
+				 * Sets the background color for the selected row in the original table
+				 */
 				function tweenStart(){
 					$(this.htmlElement.parentElement.parentElement).css("background-color", "#d9edf7");
 				}
 						
 			})(j);
+			
+			// Counting up the time counter for the animations by 2000ms
 			timeCount += 2000;
+			
+			// Remove the class usedInRow, before starting on a new row
 			$(".original-table").find(".usedInRow").removeClass("usedInRow");
 		})(i);
-			
+		
+		// Framework settings for animations
         createjs.Ticker.setFPS(60);
         createjs.Ticker.addEventListener("tick", stage);
 	}
